8a948049dd4a20ff4400e6ce924b7726937e3514
新屋一徳 <shinya@shinyapc.local>
2019-05-01 04:14:01 +0900

    upd

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
     "encoding/json"
     "fmt"
-    "io/ioutil"
+    // "io/ioutil"
     "log"
     "net/http"
     // "net/http/httputil"
@@ -13,8 +13,14 @@ import (
     "github.com/julienschmidt/httprouter"
 )
 
+type Result struct {
+    url string
+    status int
+}
+
 func ClientGet(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
     res := p.ByName("param")
+
     // ヘッダーセット
     w.Header().Set("Content-Type", "application/json")
     w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -23,65 +29,75 @@ func ClientGet(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 }
 
 func ClientPost(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-    // 終了前に閉じる
-    defer r.Body.Close()
+    r.ParseForm()
+    urls := r.Form["urls[]"]
+
+    result := []Result{}
+
+    GetResult(urls, &result, false)
+
+    log.Println(result)
+  
+    res, err := json.Marshal(result)
+    if err != nil {
+        // 変換失敗時、500エラー
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 
     // ヘッダーセット
     w.Header().Set("Content-Type", "application/json")
     w.Header().Set("Access-Control-Allow-Origin", "*")
-
-    bodyBytes, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        // 読み取り失敗時、400エラー
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
-
-    // レスポンス用の構造体
-    type ResponseParam struct {
-        ID   int    `json:"id"`
-        Name string `json:"name"`
-    }
-    var param ResponseParam
-
-    // JSON => STRUCT
-    err = json.Unmarshal(bodyBytes, &param)
-    if err != nil {
-        // 変換失敗時、400エラー
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
-
-    // STRUCT => JSON
-    res, _ := json.Marshal(param)
     // レスポンスに書き込む
     fmt.Fprintf(w, string(res))
 }
 
-func GetRequest(client *http.Client, url string)(int) {
-    req, _ := http.NewRequest("GET", url, nil)
+func GetRequest(client *http.Client, url string, ch <-chan int, wg *sync.WaitGroup, result *[]Result) {
+    
+    defer func() {
+        <- ch
+        wg.Done()
+    }()
 
+    req, _ := http.NewRequest("GET", url, nil)
     resp, err := client.Do(req)
     if err != nil {
         fmt.Println(err)
-        return 0
+        return
     }
     defer resp.Body.Close()
-    return resp.StatusCode
 
-    // dumpResp, _ := httputil.DumpResponse(resp, true)
-    // byteArray, _ := ioutil.ReadAll(resp.Body)
-    // fmt.Println(string(byteArray))
+    *result = append(*result, Result{ url: url, status: resp.StatusCode })
 }
 
 func PostRequest() {
 
 }
 
-func GetFunc(client *http.Client, url string, ch <-chan int, wg *sync.WaitGroup) {
-    log.Println(GetRequest(client, url), url)
-    <- ch
-    wg.Done()
+func GetResult(urls []string, result *[]Result, isPost bool) {
+    ch := make(chan int, 5)
+    wg := &sync.WaitGroup{}
+
+    log.Println(time.Now())
+
+    client := &http.Client{}
+
+    for _, url := range urls {
+        if url == "" {
+            continue
+        }
+        ch <- 1
+        wg.Add(1)
+        if isPost {
+            break
+        } else {
+            go GetRequest(client, url, ch, wg, result)
+        }
+        
+    }
+    wg.Wait()
+
+    log.Println(time.Now())
 }
 
 func main() {
@@ -90,35 +106,6 @@ func main() {
 
     router.GET("/Index/:param", ClientGet)
     router.POST("/Post", ClientPost)
-
-    urls := []string {
-        "https://stackoverflow.com/",
-        "http://yahoo.co.jp",
-        "https://stackoverflow.com/",
-        "http://yahoo.co.jp",
-        "https://stackoverflow.com/",
-        "http://yahoo.co.jp",
-        "https://stackoverflow.com/",
-        "http://yahoo.co.jp",
-        "https://stackoverflow.com/",
-        "http://yahoo.co.jp"}
-
-    ch := make(chan int, 5)
-    wg := &sync.WaitGroup{}
-
-    log.Println(time.Now())
-    client := &http.Client{}
-
-    for _, url := range urls {
-        ch <- 1
-        wg.Add(1)
-        go GetFunc(client, url, ch, wg)
-    }
-    wg.Wait()
-
-    log.Println(time.Now())
-
-
 
     // Webサーバーを8080ポートで立ち上げる
     err := http.ListenAndServe(":8080", router)
